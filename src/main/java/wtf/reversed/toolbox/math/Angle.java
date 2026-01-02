f971cda8b747993b3d0c158eaf3f3fb6308a9425
Jan De Kock <jan@twofold.be>
2026-01-02 14:32:11 +0100

    Add missing docs

--- src/main/java/wtf/reversed/toolbox/math/Angle.java
+++ src/main/java/wtf/reversed/toolbox/math/Angle.java
@@ -1,12 +1,27 @@
 package wtf.reversed.toolbox.math;
 
+/**
+ * Represents different angle measurement units.
+ */
 public enum Angle {
+    /**
+     * Represents angles in degrees.
+     */
     DEGREES,
+    /**
+     * Represents angles in radians.
+     */
     RADIANS;
 
     private static final float DEGREES_TO_RADIANS = (float) (Math.PI / 180.0);
     private static final float RADIANS_TO_DEGREES = (float) (180.0 / Math.PI);
 
+    /**
+     * Converts an angle to degrees.
+     *
+     * @param angle the angle to convert
+     * @return the converted angle
+     */
     public float toDegrees(float angle) {
         return switch (this) {
             case DEGREES -> angle;
@@ -14,6 +29,12 @@ public enum Angle {
         };
     }
 
+    /**
+     * Converts an angle to radians.
+     *
+     * @param angle the angle to convert
+     * @return the converted angle
+     */
     public float toRadians(float angle) {
         return switch (this) {
             case DEGREES -> angle * DEGREES_TO_RADIANS;
