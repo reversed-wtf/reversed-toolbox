87f17f3c36c6efe1ec6e66053608f9b672e09733
Jan De Kock <jan@twofold.be>
2026-05-08 20:14:44 +0200

    Add proper error handling to LZUtils

--- src/main/java/wtf/reversed/toolbox/compress/LZUtils.java
+++ src/main/java/wtf/reversed/toolbox/compress/LZUtils.java
@@ -7,28 +7,39 @@ final class LZUtils {
     }
 
     static void copyLiteral(Bytes src, int srcOff, Bytes.Mutable dst, int dstOff, int len) {
-        src.slice(srcOff, len).copyTo(dst, dstOff);
+        try {
+            src.slice(srcOff, len).copyTo(dst, dstOff);
+        } catch (IndexOutOfBoundsException e) {
+            throw new DecompressorException("Literal out of bounds: " +
+                "source=" + srcOff + ", target=" + dstOff + ", length=" + len, e);
+        }
     }
 
     static void copyReference(Bytes.Mutable dst, int dstOff, int offset, int length) {
-        if (offset <= 0 || dstOff - offset < 0) {
-            throw new DecompressorException("Match before start");
+        if (offset <= 0) {
+            throw new DecompressorException("Backreference out of bounds: " +
+                "position=" + dstOff + ", offset=" + offset + ", length=" + length);
         }
 
-        int srcPos = dstOff - offset;
-        if (offset == 1) {
-            byte b = dst.get(dstOff - 1);
-            dst.slice(dstOff, length).fill(b);
-        } else if (offset >= length) {
-            dst.slice(srcPos, length).copyTo(dst, dstOff);
-        } else {
-            dst.slice(srcPos, offset).copyTo(dst, dstOff);
-            int copied = offset;
-            while (copied < length) {
-                int chunk = Math.min(copied, length - copied);
-                dst.slice(dstOff, chunk).copyTo(dst, dstOff + copied);
-                copied += chunk;
+        try {
+            int srcPos = dstOff - offset;
+            if (offset == 1) {
+                byte b = dst.get(dstOff - 1);
+                dst.slice(dstOff, length).fill(b);
+            } else if (offset >= length) {
+                dst.slice(srcPos, length).copyTo(dst, dstOff);
+            } else {
+                dst.slice(srcPos, offset).copyTo(dst, dstOff);
+                int copied = offset;
+                do {
+                    int chunk = Math.min(copied, length - copied);
+                    dst.slice(dstOff, chunk).copyTo(dst, dstOff + copied);
+                    copied += chunk;
+                } while (copied < length);
             }
+        } catch (IndexOutOfBoundsException e) {
+            throw new DecompressorException("Backreference out of bounds: " +
+                "position=" + dstOff + ", offset=" + offset + ", length=" + length);
         }
     }
 }
